8b7d6325773b47cc6b6be70bc509557ee0dd5d5f
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-19 03:39:07 -0500

    test: Updates tests for latest changes.

--- tests/FixedHeaderLayout.test.tsx
+++ tests/FixedHeaderLayout.test.tsx
@@ -1,59 +1,76 @@
+/** @jsx jsx */
 import React, { ReactElement } from "react"
 import { ViewportProvider } from "@alexseitsinger/react-viewport-container"
-import { CSSObject } from "@emotion/core"
+import { jsx } from "@emotion/core"
 import styled from "@emotion/styled"
 import { mount } from "enzyme"
 import { matchers } from "jest-emotion"
 
 expect.extend(matchers)
 
-import { FixedHeader, FixedHeaderLayout, withFixedHeaderLayout } from "src"
-import { HeaderInner } from "src/elements"
-
-interface AppProps {
-  initialHeight: string;
-  headerStyle: CSSObject;
-}
+import {
+  FixedHeader,
+  FixedHeaderLayout,
+  PageContainer,
+  withFixedHeaderLayout,
+} from "src"
+import { Container, HeaderInner } from "src/elements"
 
 function HeaderBody(): ReactElement {
   return <div>Header</div>
 }
 
-interface PageBodyProps {
-  mainHeight: string;
-  fullHeight: string;
-}
-
-const Body = styled.div``
-
-const PageBody = withFixedHeaderLayout(
-  ({ mainHeight, fullHeight }: PageBodyProps): ReactElement => (
-    <Body style={{ height: mainHeight, maxHeight: fullHeight }}>Body</Body>
-  )
-)
-
-const App = ({ headerStyle, initialHeight }: AppProps): ReactElement => (
-  <ViewportProvider initialHeight={"600px"} initialWidth={"600px"}>
-    <FixedHeaderLayout
-      renderHeader={(): ReactElement => <HeaderBody />}
-      renderBody={(): ReactElement => <PageBody />}
-      headerStyle={headerStyle}
-      initialHeight={initialHeight}
-    />
-  </ViewportProvider>
-)
-
 describe("FixedHeaderLayout", () => {
-  it("should render correctly", () => {
-    const wrapper = mount(<App headerStyle={{}} initialHeight={"40px"} />)
+  it("should pass height props to component wrapped with withFixedHeaderLayout()", () => {
+    const Body = styled.div``
+
+    interface PageBodyProps {
+      mainHeight: string;
+      fullHeight: string;
+    }
+
+    const PageBody = withFixedHeaderLayout(
+      ({ mainHeight, fullHeight }: PageBodyProps): ReactElement => {
+        return (
+          <Body css={{ height: mainHeight, maxHeight: fullHeight }}>Body</Body>
+        )
+      }
+    )
+
+    const App = (): ReactElement => (
+      <ViewportProvider initialHeight={"600px"} initialWidth={"600px"}>
+        <FixedHeaderLayout
+          initialHeight={"40px"}
+          renderHeader={(): ReactElement => <HeaderBody />}
+          renderBody={(): ReactElement => <PageBody />}
+        />
+      </ViewportProvider>
+    )
+
+    const wrapper = mount(<App />)
 
     expect(wrapper.find(FixedHeader)).toHaveLength(1)
     expect(wrapper.find(HeaderInner)).toHaveStyleRule("height", "40px")
     expect(wrapper.find(PageBody)).toHaveLength(1)
+    expect(wrapper.find(Body)).toHaveStyleRule("height", "560px")
+    expect(wrapper.find(Body)).toHaveStyleRule("max-height", "600px")
+  })
 
-    const style = wrapper.find(Body).prop("style")
+  it("should render page container with the remaining viewport height", () => {
+    const App = (): ReactElement => (
+      <ViewportProvider initialHeight={"600px"} initialWidth={"600px"}>
+        <FixedHeaderLayout
+          initialHeight={"40px"}
+          renderHeader={(): ReactElement => <HeaderBody />}
+          renderBody={(): ReactElement => (
+            <PageContainer>Content</PageContainer>
+          )}
+        />
+      </ViewportProvider>
+    )
 
-    expect(style).toHaveProperty("height", "560px")
-    expect(style).toHaveProperty("maxHeight", "600px")
+    const wrapper = mount(<App />)
+
+    expect(wrapper.find(Container)).toHaveStyleRule("height", "560px")
   })
 })
