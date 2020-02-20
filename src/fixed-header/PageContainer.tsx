6b39da8058b2989ea4b5a3fd09ba1be792b5ae7b
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-02-20 00:54:54 -0500

    feat: Allows for optional static height.

--- src/fixed-header/PageContainer.tsx
+++ src/fixed-header/PageContainer.tsx
@@ -1,17 +1,22 @@
 /** @jsx jsx */
 import React, { ReactElement, ReactNode } from "react"
-import { jsx } from "@emotion/core"
+import { CSSObject, jsx } from "@emotion/core"
 
 import { Container } from "./elements"
 import { withFixedHeaderLayout } from "./withFixedHeaderLayout"
 
 interface Props {
+  isStatic: boolean;
   mainHeight: string;
   children: ReactNode | ReactNode[];
 }
 
 export const PageContainer = withFixedHeaderLayout(
-  ({ mainHeight, children }: Props): ReactElement => (
-    <Container css={{ minHeight: mainHeight }}>{children}</Container>
-  )
+  ({ mainHeight, isStatic, children }: Props): ReactElement => {
+    let style: CSSObject = { minHeight: mainHeight }
+    if (isStatic) {
+      style = { height: mainHeight }
+    }
+    return <Container css={style}>{children}</Container>
+  }
 )
