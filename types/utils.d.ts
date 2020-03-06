ec83fbcd82b204ef544ce62a87ba238a973ede12
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2020-03-06 02:10:28 -0500

    refactor: Updates types.

--- types/utils.d.ts
+++ types/utils.d.ts
@@ -1,3 +1,4 @@
+export declare const getSize: (n: string | number) => number;
 export declare const isNode: boolean;
 export declare const isBrowser: boolean;
 export declare const isNullish: (o?: any) => boolean;
